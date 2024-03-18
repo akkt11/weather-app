3aeba9216581f41f2d0fc721fbab94d6ac100bc3
akktii <aktanmoldokeev123@gmail.com>
2024-03-18 15:35:45 +0600

    changed to recharts & refactor layout

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -1,90 +1,16 @@
 import { FC } from "react";
 import { Typography } from "../../shared/ui/Typography/Typography";
-import { Line } from "react-chartjs-2";
-import { Chart } from "chart.js";
-import ChartDataLabels from "chartjs-plugin-datalabels";
-import "chart.js/auto";
-import style from "./Forecast.module.scss";
 import { Clock } from "../../icons/Clock";
-import type { ChartData, ChartOptions } from "chart.js";
-Chart.register(ChartDataLabels);
+import style from "./Forecast.module.scss";
+import { Line, LineChart } from "recharts";
+import { Button } from "../../shared/ui/Button/Button";
 
 export const Forecast: FC = () => {
-  const forecast = [
-    { hour: 1, degree: 10 },
-    { hour: 2, degree: 20 },
-    { hour: 3, degree: 15 },
-    { hour: 4, degree: 25 },
-    { hour: 5, degree: 22 },
-    { hour: 6, degree: 30 },
-    { hour: 8, degree: 50 },
-    { hour: 9, degree: 50 },
-    { hour: 10, degree: 25 },
-    { hour: 11, degree: 11 },
-    { hour: 12, degree: 32 },
-    { hour: 13, degree: 50 },
-    { hour: 14, degree: 1 },
-    { hour: 15, degree: 22 },
-    { hour: 16, degree: 45 },
-    { hour: 17, degree: 2 },
-    { hour: 18, degree: 5 },
-    { hour: 19, degree: 6 },
-    { hour: 20, degree: 34 },
-    { hour: 21, degree: 23 },
-    { hour: 22, degree: 1 },
-    { hour: 23, degree: 0 },
-    { hour: 24, degree: 12 },
-  ];
+  const data = Array.from({ length: 24 }).map((_, index) => ({
+    hour: index,
+    degree: Math.floor(Math.random() * 30),
+  }));
 
-  const hours = forecast.map((item) => item.hour);
-  const degrees = forecast.map((item) => item.degree);
-
-  const options: ChartOptions<"line"> = {
-    responsive: true,
-    elements: {
-      line: {
-        tension: 0.4,
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        enabled: false,
-      },
-      datalabels: {
-        color: "#fff",
-        font: {
-          size: 14,
-        },
-        align: "top",
-        formatter: (value: number) => {
-          return `${value}°`;
-        },
-      },
-    },
-    scales: {
-      x: {
-        display: false,
-      },
-      y: {
-        display: false,
-      },
-    },
-  };
-
-  const data: ChartData<"line"> = {
-    labels: hours,
-    datasets: [
-      {
-        data: degrees,
-        borderColor: "#FFC355",
-        backgroundColor: "#FFC355",
-        pointStyle: false,
-      },
-    ],
-  };
   return (
     <div className={style.forecast}>
       <div className={style.forecastTime}>
@@ -95,8 +21,19 @@ export const Forecast: FC = () => {
       </div>
 
       <div className={style.forecastChart}>
-        <Line options={options} data={data} />
+        <LineChart
+          width={2400}
+          height={100}
+          data={data}
+          style={{ position: "absolute" }}
+        >
+          <Line type="monotone" dataKey="degree" stroke="#FFC355" />
+        </LineChart>
       </div>
+
+      <Button type="primary" customClasses={style.forecastButton}>
+        <Typography variant="h6">5-day forecast</Typography>
+      </Button>
     </div>
   );
 };
--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -18,51 +18,41 @@ export const Overview: FC = () => {
     <section className={style.overview}>
       <div className={style.overviewContainer}>
         <div className={style.overviewInfo}>
-          <div className={style.info}>
-            <div className={style.infoMain}>
-              <div className={style.infoCity}>
-                <div className={style.city}>
-                  <Location />
+          <div className={style.infoWrapper}>
+            <div className={style.action}>
+              <div className={style.actionCity}>
+                <Location />
 
-                  <Typography
-                    variant="h3"
-                    weight="medium"
-                    className={style.cityTitle}
-                  >
-                    {location}
-                  </Typography>
+                <Typography variant="h3" weight="medium">
+                  {location}
+                </Typography>
 
-                  <Button type="icon">
-                    <Chevron type="right" />
-                  </Button>
-                </div>
+                <Button type="icon">
+                  <Chevron type="right" />
+                </Button>
+              </div>
 
+              <div className={style.actionImage}>
                 <img
                   src="https://cdnn11.img.sputnik.by/img/104429/09/1044290984_433:0:2481:2048_1920x0_80_0_0_d1ec1f648af8425ae6f73734c93b85e2.jpg"
                   alt="Ava"
                 />
               </div>
+            </div>
 
-              <Typography
-                variant="h2"
-                weight="medium"
-                className={style.infoTitle}
-              >
+            <div className={style.main}>
+              <Typography variant="h2" weight="medium">
                 {weatherData?.weather[0].main}
               </Typography>
-            </div>
 
-            <div className={style.infoImage}>
-              <CloudSun />
-            </div>
+              <div className={style.mainImage}>
+                <CloudSun />
+              </div>
 
-            <div className={style.infoDescription}>
-              <div>
+              <div className={style.mainDescription}>
                 <Typography variant="h1" weight="medium">
                   {`${weatherData?.temp.day}°C`}
                 </Typography>
-              </div>
-              <div>
                 <Typography variant="h4">
                   {`${dateToWeekDay(weatherData?.dt)} | ${dateToDMY(
                     weatherData?.dt
