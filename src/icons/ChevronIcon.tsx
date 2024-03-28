8b59b31ee334869e426a3f3755729adaec8f5dee
akktii <aktanmoldokeev123@gmail.com>
2024-03-28 18:31:16 +0600

    fix chevron svg

--- src/icons/ChevronIcon.tsx
+++ src/icons/ChevronIcon.tsx
@@ -3,19 +3,19 @@ import { FC } from "react";
 export const ChevronIcon: FC = () => {
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
       width="21"
       height="36"
-      viewBox="0 0 14 38"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 14 38"
     >
       <path
-        d="M4 28L13 18.5L4 9"
-        stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 28l9-9.5L4 9"
+      ></path>
     </svg>
   );
 };
